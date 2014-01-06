e31b9a2b07bd4ec233cb96959ed13fc4ba3eec5f
Max Luzuriaga <max@luzuriaga.com>
2014-01-05 19:23:26 -0500

    Added wildcard ability in routing, restructure of routes.rb.

--- config/routes.js
+++ config/routes.js
@@ -3,8 +3,21 @@ var posts_controller = require("../app/controllers/posts_controller");
 // TODO: Get rid of adminOnly option in router.js
 
 module.exports = {
-  "/": {
+  '': {
     action: posts_controller.index,
     accept: ["GET"]
+  },
+  'posts': {
+    action: posts_controller.index,
+    accept: ["GET"],
+
+    'new': {
+      // action: posts_controller.new,
+      accept: ["GET"]
+    },
+    ':id': {
+      // action: posts_controller.show,
+      accept: ["GET"],
+    }
   }
 }
