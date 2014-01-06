fc6ae26780484a7640097ee7f456d592e57322e4
Max Luzuriaga <max@luzuriaga.com>
2014-01-05 19:23:47 -0500

    Removed finished TODO.

--- config/routes.js
+++ config/routes.js
@@ -1,7 +1,5 @@
 var posts_controller = require("../app/controllers/posts_controller");
 
-// TODO: Get rid of adminOnly option in router.js
-
 module.exports = {
   '': {
     action: posts_controller.index,
