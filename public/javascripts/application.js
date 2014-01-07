66e356cb812a533fee5db9e549bc78db92eeb911
Max Luzuriaga <max@luzuriaga.com>
2014-01-06 21:51:59 -0500

    New post form, styled.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -6,6 +6,35 @@ function sizeTitles() {
 
 $(function() {
 	sizeTitles();
+	$('textarea').elastic();
+	$('textarea').css({ resize: 'none' });
+
+	var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	var target = "friendscentral.org";
+
+	$('.postForm').submit(function(e) {
+		var email = $("p.email input").val();
+		if (!(re.test(email) && (email.substr(email.length - target.length) == target))) {
+			$('.postForm p.email').addClass("error");
+			e.preventDefault();
+		}
+
+		var title = $("p.title input").val();
+		if (title == "") {
+			$('.postForm p.title').addClass("error");
+			e.preventDefault();
+		}
+
+		var text = $("p.text textarea").val();
+		if (text == "") {
+			$('.postForm p.text').addClass("error");
+			e.preventDefault();
+		}
+	});
+
+	$("input, textarea").keypress(function() {
+		$(this).parent().removeClass("error");
+	});
 });
 
 $(window).resize(sizeTitles);
