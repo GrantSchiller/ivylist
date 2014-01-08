9c42f96c603d4d93a84de9a82b49a637d512d93e
Max Luzuriaga <max@luzuriaga.com>
2014-01-08 17:56:21 -0500

    Installed mongoose, rewrote lib/db.js

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 var server = require ("./lib/server"),
+    db = require("./lib/db"),
     router = require("./lib/router"),
     routes = require("./config/routes");
 
-server.start(router.route, routes, __dirname + '/public');
+db.connect(function() {
+  server.start(router.route, routes, __dirname + '/public');
+});
