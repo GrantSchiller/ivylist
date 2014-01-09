bea3cc989483b09ec6819435e384a0172027690e
Max Luzuriaga <max@luzuriaga.com>
2014-01-08 19:34:58 -0500

    Reorganized Post model to group class and instance methods.

--- app/models/post.js
+++ app/models/post.js
@@ -29,45 +29,49 @@ var post = new mongoose.Schema({
   date: { type: Date, default: Date.now }
 });
 
-post.methods.prettyDate = function(format) {
-  return dateformat(this.date, format);
-};
-
-post.methods.generateConfirmationCode = function() {
-  var text = (new Date()) + "--" + this.email + "--" + this.title + "--" + this.text_markdown;
-
-  var shasum = crypto.createHash('sha1');
-  shasum.update(text);
-  this.confirmation_code = shasum.digest('hex');
-};
-
-post.methods.formatText = function(callback) {
-  marked(this.text_markdown, function(err, result) {
-    if (err) throw err;
-    this.text_formatted = result;
-    
-    callback();
-  }.bind(this));
-};
-
 post.pre('save', function(next) {
   this.generateConfirmationCode();
   this.formatText(next);
 });
 
-post.statics.numPages = function(perPage, callback) {
-  this.count({ confirmed: true }, function(err, count) {
-    callback(Math.ceil(count/perPage));
-  });
-}
+post.methods = {
+  prettyDate: function(format) {
+    return dateformat(this.date, format);
+  },
 
-post.statics.findPostsOnPage = function(currentPage, totalPages, perPage, callback) {
-  return this.find({ confirmed: true }).sort({date: -1}).skip(perPage * (currentPage - 1)).limit(perPage);
+  generateConfirmationCode: function() {
+    var text = (new Date()) + "--" + this.email + "--" + this.title + "--" + this.text_markdown;
+
+    var shasum = crypto.createHash('sha1');
+    shasum.update(text);
+    this.confirmation_code = shasum.digest('hex');
+  },
+
+  formatText: function(callback) {
+    marked(this.text_markdown, function(err, result) {
+      if (err) throw err;
+      this.text_formatted = result;
+      
+      callback();
+    }.bind(this));
+  }
 };
 
-post.statics.findByIdString = function(id) {
-  var newID = mongoose.Types.ObjectId(id);
-  return this.findById(newID);
+post.statics = {
+  numPages: function(perPage, callback) {
+    this.count({ confirmed: true }, function(err, count) {
+      callback(Math.ceil(count/perPage));
+    });
+  },
+
+  findPostsOnPage: function(currentPage, totalPages, perPage, callback) {
+    return this.find({ confirmed: true }).sort({date: -1}).skip(perPage * (currentPage - 1)).limit(perPage);
+  },
+
+  findByIdString: function(id) {
+    var newID = mongoose.Types.ObjectId(id);
+    return this.findById(newID);
+  }
 };
 
 module.exports = mongoose.model('Post', post);
