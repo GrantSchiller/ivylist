db540aa8956c0ad1b9c7f9e135a9d2da0234031b
Max Luzuriaga <max@luzuriaga.com>
2014-01-18 15:24:39 -0500

    Post controller use post url helper, fixed issue with signed-in user sending email.

--- app/controllers/posts_controller.js
+++ app/controllers/posts_controller.js
@@ -8,7 +8,7 @@ var perPage = 20;
 
 function _findPost(id, request, response, callback) {
   if (id && id.length == 24) {
-    Post.findByIdString(id).exec(function(err, post) {
+    Post.findByIdString(id).populate('_category').exec(function(err, post) {
       if (post) {
         callback(post);
       } else {
@@ -86,7 +86,7 @@ function create(response, request, params, postData, sockets) {
           if (err) {
             helper.redirectTo("/new", request, response); // TODO: display feedback to user for why post was invalid
           } else {
-            helper.redirectTo("/posts/" + post.id, request, response);
+            helper.redirectTo(post.url(), request, response);
 
             helper.renderPartial("posts/_post", { post: post }, function(content) {
               sockets.emit('new post', { post: content });
@@ -139,12 +139,22 @@ function contact(response, request, params, postData) {
 function sendEmail(response, request, params, postData) {
   var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   var target = "friendscentral.org";
-  if (re.test(postData.email) && (postData.email.substr(postData.email.length - target.length) == target) && (postData.email_text.trim().length != 0)) {
+  if (postData.email_text.trim().length != 0) {
     _findPost(params.id, request, response, function(post) {
       var toEmail = post._user ? post._user.email : post.email;
-      
+      var fromEmail;
+
+      if (request.user) {
+        fromEmail = request.user.email;
+      } else if (re.test(postData.email) && (postData.email.substr(postData.email.length - target.length) == target)) {
+        fromEmail = postData.email;
+      } else {
+        helper.renderError(403, request, response);
+        return;
+      }
+
       mail({
-        from: postData.email,
+        from: fromEmail,
         to: toEmail,
         subject: "Re: " + post.title,
         text: postData.email_text,
@@ -161,10 +171,10 @@ function sendEmail(response, request, params, postData) {
 }
 
 function confirm(response, request, params, postData, sockets) {
-  Post.findOne({ confirmation_code: params.confirmation_code }, function(err, post) {
+  Post.findOne({ confirmation_code: params.confirmation_code }).populate('_category').exec(function(err, post) {
     if (post) {
       if (post.confirmed) {
-        helper.redirectTo("/posts/" + post.id, request, response);
+        helper.redirectTo(post.url(), request, response);
       } else {
         post.update({ confirmed: true }, function(err) {
           if (!request.user && request.session.email && request.session.email == post.email) {
@@ -172,7 +182,7 @@ function confirm(response, request, params, postData, sockets) {
             helper.render("posts/confirm.html", { email: request.session.confirmedEmail, post: post }, request, response, 200);
           } else {
             // Confirming from different device
-            helper.redirectTo("/posts/" + post.id, request, response);
+            helper.redirectTo(post.url(), request, response);
           }
 
           helper.renderPartial("posts/_post", { post: post }, function(content) {
