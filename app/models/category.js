30b672031acd1bb497d0c08c164835e71bc608b1
Max Luzuriaga <max@luzuriaga.com>
2014-01-18 12:58:34 -0500

    Post#findPosts method.

--- app/models/category.js
+++ app/models/category.js
@@ -2,11 +2,14 @@ var mongoose = require('mongoose'),
     Post = require('./post');
 
 var category = new mongoose.Schema({
+  slug: { type: String, required: true },
   name: { type: String, required: true }
 });
 
 category.methods = {
-
+  findPosts: function() {
+    return Post.find({ confirmed: true, category: this.slug });
+  }
 };
 
 category.statics = {
